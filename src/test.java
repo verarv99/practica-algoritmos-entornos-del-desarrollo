f5b5e34be16a7895907e6bdde3154442c3f984d0
verarv99 <veravr99@gmail.com>
2024-04-18 12:31:57 +0200

    test

---
+++ src/test.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import util.Algo;
+class test {
+
+	@Test
+	void test() {
+	System.out.println(Algo.calcularFactorial(1));
+	System.out.println(Algo.calcularFactorial(4));
+	System.out.println(Algo.calcularFactorial(7));
+	System.out.println(Algo.esPrimo(7));
+	System.out.println(Algo.esPrimo(8));
+	System.out.println(Algo.esPrimo(9));
+	System.out.println(Algo.fibonacci(7));
+	System.out.println(Algo.fibonacci(10));
+	System.out.println(Algo.fibonacci(5));
+	}
+
+}
