9bc44ca6817ed433a46407560097a8eb2787d6ee
verarv99 <veravr99@gmail.com>
2024-04-18 10:52:30 +0200

    factorial

--- src/algoritmos.java
+++ src/algoritmos.java
@@ -14,5 +14,12 @@ public abstract class algoritmos {
         }
  
 }
+	 public static int calcularFactorial(int n) {
+	        if (n == 0) {
+	            return 1;
+	        } else {
+	            return n * calcularFactorial(n - 1);
+	        }
+	    }
 
 }
